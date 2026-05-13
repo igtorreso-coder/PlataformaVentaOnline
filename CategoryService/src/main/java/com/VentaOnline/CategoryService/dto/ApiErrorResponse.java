aab9615038b68def7ad629c17fc2c893d4dc56fd
Ignacio Torres <ignaciotorresolivares@gmail.com>
2026-05-13 12:37:12 -0400

    Añadidos constructores por defecto y con parámetros a la clase ApiErrorResponse en múltiples servicios.

--- CategoryService/src/main/java/com/VentaOnline/CategoryService/dto/ApiErrorResponse.java
+++ CategoryService/src/main/java/com/VentaOnline/CategoryService/dto/ApiErrorResponse.java
@@ -2,11 +2,15 @@ package com.VentaOnline.CategoryService.dto;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ApiErrorResponse {
     private LocalDateTime timestamp;
     private Integer status;
