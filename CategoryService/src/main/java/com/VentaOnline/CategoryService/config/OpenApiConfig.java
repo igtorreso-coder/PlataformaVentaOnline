1e4a42984b9dd2323059c6777b4f5a28500c2032
IgnacioTorres <ig.torreso@duocuc.cl>
2026-06-19 12:59:54 -0400

    CategoryService: corrige encoding y descripcion en OpenApiConfig

--- CategoryService/src/main/java/com/VentaOnline/CategoryService/config/OpenApiConfig.java
+++ CategoryService/src/main/java/com/VentaOnline/CategoryService/config/OpenApiConfig.java
@@ -1,9 +1,7 @@
 package com.VentaOnline.CategoryService.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,14 +12,8 @@ public class OpenApiConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Category Service API")
-                        .description("Microservicio de Categorías - CRUD de categorías e integración con microservicio de usuarios")
-                        .version("1.0.0")
-                        .contact(new Contact()
-                                .name("Plataforma Venta Online")
-                                .email("soporte@ventaonline.com"))
-                        .license(new License()
-                                .name("Apache 2.0")
-                                .url("http://springdoc.org")));
+                        .title("Category Service")
+                        .description("Gestión de categorías de productos")
+                        .version("1.0.0"));
     }
 }
