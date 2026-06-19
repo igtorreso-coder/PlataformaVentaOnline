d0aedcc53bd2a95637063c2c6b5a182e7ef08e9d
IgnacioTorres <ig.torreso@duocuc.cl>
2026-06-18 23:34:47 -0400

    DiscountService: eliminar código muerto y agregar OpenAPI

---
+++ DiscountService/src/main/java/com/VentaOnline/DiscountService/config/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package com.VentaOnline.DiscountService.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Discount Service API")
+                        .description("Microservicio de Descuentos y Cupones - CRUD de cupones, validación y uso de descuentos")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Plataforma Venta Online")
+                                .email("soporte@ventaonline.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://springdoc.org")));
+    }
+}
