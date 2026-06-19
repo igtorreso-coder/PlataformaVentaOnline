b0aeacb2c59dc34a91a36f8073c0fc37e4c9b3da
IgnacioTorres <ig.torreso@duocuc.cl>
2026-06-19 12:59:57 -0400

    ShippingService: corrige encoding y descripcion en OpenApiConfig

--- ShippingService/src/main/java/com/VentaOnline/ShippingService/config/OpenApiConfig.java
+++ ShippingService/src/main/java/com/VentaOnline/ShippingService/config/OpenApiConfig.java
@@ -1,9 +1,7 @@
 package com.VentaOnline.ShippingService.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,14 +12,8 @@ public class OpenApiConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Shipping Service API")
-                        .description("Microservicio de Envíos - Gestión de envíos, actualización de estados y seguimiento por pedido")
-                        .version("1.0.0")
-                        .contact(new Contact()
-                                .name("Plataforma Venta Online")
-                                .email("soporte@ventaonline.com"))
-                        .license(new License()
-                                .name("Apache 2.0")
-                                .url("http://springdoc.org")));
+                        .title("Shipping Service")
+                        .description("Gestión de envíos y logística")
+                        .version("1.0.0"));
     }
 }
