1b5ae5b30f30b5fc97478c5b37fc057c5ec070af
IgnacioTorres <ig.torreso@duocuc.cl>
2026-06-19 12:59:56 -0400

    OrderService: corrige encoding y descripcion en OpenApiConfig

--- OrderService/src/main/java/com/VentaOnline/OrderService/config/OpenApiConfig.java
+++ OrderService/src/main/java/com/VentaOnline/OrderService/config/OpenApiConfig.java
@@ -1,9 +1,7 @@
 package com.VentaOnline.OrderService.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,14 +12,8 @@ public class OpenApiConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Order Service API")
-                        .description("Microservicio de Pedidos - Gestión de pedidos, detalles, estados y consulta por usuario")
-                        .version("1.0.0")
-                        .contact(new Contact()
-                                .name("Plataforma Venta Online")
-                                .email("soporte@ventaonline.com"))
-                        .license(new License()
-                                .name("Apache 2.0")
-                                .url("http://springdoc.org")));
+                        .title("Order Service")
+                        .description("Gestión de pedidos y detalles")
+                        .version("1.0.0"));
     }
 }
